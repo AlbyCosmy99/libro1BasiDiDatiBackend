01ca1346ed644abeb5ac185960efe75c6c5d76fb
Andrei <dinamo1999@icloud.com>
2024-07-30 00:32:14 +0200

    changes

--- database/schemas/pageSchema.js
+++ database/schemas/pageSchema.js
@@ -13,7 +13,7 @@ const pageSchema = Schema({
         default: /*html*/`
             <div class="d-flex flex-column justify-content-center align-items-center" style="flex:1">
                 <h1 style="color: red;">PAGE JUST CREATED. EDIT IT!<h1>
-                <button id="selectButton" style="font-size: 0.6rem; background-color: orange;margin-top: 1px" onclick="selectOption(event, name)">EDIT</button>
+                <button id="selectButton" style="font-size: 0.6rem; background-color: orange;margin-top: 1px" onclick="selectOption(event, 'Daniel')">EDIT</button>
             </div>
         `
     },
